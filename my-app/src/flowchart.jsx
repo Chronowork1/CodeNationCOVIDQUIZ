3dba0106c57c2130a222dbcec83b701db8429538
Chronowork1 <jianxie6464@gmail.com>
2020-04-17 02:16:19 +0000

    Included React, flowchart, question and modified app

---
+++ my-app/src/flowchart.jsx
@@ -0,0 +1,18 @@
+export const Flowchart = [
+    {
+        question: "True of False, mask prevent contraction of covid.",
+        answers: [
+            "true",
+            "false"
+        ]
+    },
+    {
+        question: "What is the official name of the coronavirus?",
+        answers: {
+            0: "Covid-10",
+            1: "Covid-19",
+            2: "Coronavirus",
+            3: "CV"
+        }
+    }
+]
